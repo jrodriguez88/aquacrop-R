e50427d8c6846f4c78f64756c7b3094d04e61253
jrodriguez88 <jeferson_88@hotmail.com>
2019-05-30 20:50:00 -0500

    calculate ETo and make weather files

--- make_weather_aquacrop.R
+++ make_weather_aquacrop.R
@@ -5,18 +5,114 @@
 
 ### Load packages
 library(tidyverse)
+library(data.table)
 library(sirad)
 library(lubridate)
 
 ### read_data from csv. format (oryza)
+path <- paste0(getwd(), "/outputs/")
+lat <- 3.5
+lon <- -75.5
+alt <- 22
+wvel <- 2
+loc <- "BOGU"
+co2_file <- "MaunaLoa.CO2"
 
-### Cal ETo 
-vp <- es(30.2, 24.3)
-date <- make_date(1998,1,3)
+wth_data <- fread("data/Boerasire_Region3.txt", col.names = c("rain", "srad", "tmax", "tmin")) %>% as_tibble() %>%
+    mutate(date = seq.Date(make_date(1998, 1, 1),
+                           make_date(2018, 12, 31), "days")) %>%
+    select(date, everything())
 
-et0(29.6, 24.7, vp, 20, 0.95, 10, 2, 2, NA, date, 3.5)
+### Cal ETo
 
+ETo_cal <- function(wth_data, lat, lon, alt, wvel){
+    
+    ## Estimate clear sky transmissivity
+    extraT <- extrat(lubridate::yday(wth_data$date), lat)$ExtraTerrestrialSolarRadiationDaily
+    
+    ## cal trasmisivity 3% days
+    tal <- cst(wth_data$srad, wth_data$date, radians(lat), extraT, 3)
+    
+    ETo <- wth_data %>%
+        mutate(
+            vp = sirad::es(tmax, tmin), 
+            extraT = extraT, 
+            ETo = sirad::et0(tmax, tmin, vp, srad, tal, alt, winds, 2, extraT)
+        ) %>%
+        pull(ETo)
+    
+}
+
+ETo <- ETo_cal(wth_data, lat, lon, alt, wvel)
 
 ## Split data and write .ETo / .PLU / Tnx / .CLI files.
 
+# Climate file .CLI
+write_CLI <- function(path){
+    sink(file = paste0(path, loc, ".CLI"), append = F)   
+cat(paste(loc), sep = "\n")
+cat(" 6.0   : AquaCrop Version (March 2017)", sep = "\n")
+cat(paste0(loc, ".Tnx"), sep = "\n")
+cat(paste0(loc, ".ETo"), sep = "\n")
+cat(paste0(loc, ".PLU"), sep = "\n")
+cat(paste(co2_file), sep = "\n")
 
+sink()    
+
+}
+write_CLI(path)
+
+# Temperature file .Tnx
+write_Tnx <- function(path){
+    sink(file = paste0(path, loc, ".Tnx"), append = F)   
+cat(paste0(loc, " : daily temperature data (", format(min(wth_data$date), "%d %B %Y"), " - ", format(max(wth_data$date), "%d %B %Y"), ")"))
+cat("\n")
+cat(paste0("     1  : Daily records (1=daily, 2=10-daily and 3=monthly data)"), sep = "\n")
+cat(paste0("     ", day(min(wth_data$date)), "  : First day of record (1, 11 or 21 for 10-day or 1 for months)") , sep = "\n")
+cat(paste0("     ", month(min(wth_data$date)), "  : First month of record"), sep = "\n")
+cat(paste0("  ", year(min(wth_data$date)), "  : First year of record (1901 if not linked to a specific year)") , sep = "\n")
+cat("\n")
+cat(paste0("  Tmin (C)   TMax (C)", sep = "\n"))
+cat("=======================", sep = "\n")
+write.table(data.frame(tmin = sprintf("%10.1f", wth_data$tmin),
+                       tmax = sprintf("%10.1f", wth_data$tmax)), 
+            row.names = F, quote = F, col.names = F)
+
+sink()
+
+}
+write_Tnx(path)
+
+write_PLU <- function(path){
+    sink(file = paste0(path, loc, ".PLU"), append = F)
+cat(paste0(loc, " : daily rainfall data (", format(min(wth_data$date), "%d %B %Y"), " - ", format(max(wth_data$date), "%d %B %Y"), ")"))
+cat("\n")
+cat(paste0("     1  : Daily records (1=daily, 2=10-daily and 3=monthly data)"), sep = "\n")
+cat(paste0("     ", day(min(wth_data$date)), "  : First day of record (1, 11 or 21 for 10-day or 1 for months)") , sep = "\n")
+cat(paste0("     ", month(min(wth_data$date)), "  : First month of record"), sep = "\n")
+cat(paste0("  ", year(min(wth_data$date)), "  : First year of record (1901 if not linked to a specific year)") , sep = "\n")
+cat("\n")
+cat(paste0("  Total Rain (mm)", sep = "\n"))
+cat("=======================", sep = "\n")
+writeLines(sprintf("%10.1f", wth_data$rain))
+sink()
+
+}
+write_PLU(path)
+
+write_ETo <- function(path){
+    sink(file = paste0(path, loc, ".ETo"), append = F)
+    cat(paste0(loc, " : daily ETo data (", format(min(wth_data$date), "%d %B %Y"), " - ", format(max(wth_data$date), "%d %B %Y"), ")"))
+    cat("\n")
+    cat(paste0("     1  : Daily records (1=daily, 2=10-daily and 3=monthly data)"), sep = "\n")
+    cat(paste0("     ", day(min(wth_data$date)), "  : First day of record (1, 11 or 21 for 10-day or 1 for months)") , sep = "\n")
+    cat(paste0("     ", month(min(wth_data$date)), "  : First month of record"), sep = "\n")
+    cat(paste0("  ", year(min(wth_data$date)), "  : First year of record (1901 if not linked to a specific year)") , sep = "\n")
+    cat("\n")
+    cat(paste0("  Average ETo (mm/day)", sep = "\n"))
+    cat("=======================", sep = "\n")
+    writeLines(sprintf("%10.1f", ETo))
+    sink()
+    
+}
+write_ETo(path) 
