73901269a589669438759b1f910b1531cba23205
jrodriguez88 <jeferson_88@hotmail.com>
2019-08-23 10:21:41 -0500

    Function to create aquacrop soil file

--- make_soil_aquacrop.R
+++ make_soil_aquacrop.R
@@ -4,8 +4,44 @@
 # 2019
 
 ### Load packages
-
+library(tidyverse)
 
 ### Read data from csv 
 
+data <- read.csv("data/soil_to_aquacrop.csv")
+id_name <- "soilname"
+CN <- 72
+REW <- 11
 ### Function to write . SOL files
+
+make_soil_aquacrop <- function(id_name, data, CN, REW, fromSoilGrids = T, model_version = 6.1) {
+    
+    data <- as.data.frame(data)
+
+    sink(paste0(id_name, ".SOL"), F)    
+    cat(paste0(id_name, " AquaCrop soil file - by https://github.com/jrodriguez88"))
+    cat('\n')
+    cat(paste0("        ", model_version,"                 : AquaCrop Version (May 2018)"), sep = "\n")
+    cat(paste0("       ", CN, "                   : CN (Curve Number)") , sep = "\n")
+    cat(paste0("       ", REW, "                   : Readily evaporable water from top layer (mm)"), sep = "\n")
+    cat(paste0("        ", nrow(data), "                   : number of soil horizons") , sep = "\n")
+    cat(paste0("       -9                   : variable no longer applicable"), sep = "\n")
+    cat(paste0("  Thickness  Sat   FC    WP     Ksat   Penetrability  Gravel  CRa       CRb           description"), sep = "\n")
+    cat(paste0("  ---(m)-   ----(vol %)-----  (mm/day)      (%)        (%)    -----------------------------------------"), sep = "\n")
+    write.table(data.frame(Thickness    = sprintf("%8.2f", data[["Thickness"]]    ),
+                           Sat          = sprintf("%7.1f", data[["Sat"]]          ),
+                           FC           = sprintf("%5.1f", data[["FC"]]           ),
+                           WP           = sprintf("%5.1f", data[["WP"]]           ),
+                           Ksat         = sprintf("%7.1f", data[["Ksat"]]         ),
+                           Penetrability= sprintf("%10.0f",data[["Penetrability"]]),
+                           Gravel       = sprintf("%9.0f", data[["Gravel"]]       ),
+                           CRa          = sprintf("%13.6f",data[["CRa"]]          ),
+                           CRb          = sprintf("%9.6f", data[["CRb"]]          ),
+                           description  = sprintf("%16s",  data[["description"]]  )),
+                row.names = F, quote = F, col.names = F)
+    sink()
+    
+}
+
+#make_soil_aquacrop(id_name, data, CN, REW, fromSoilGrids = T, model_version = 6.1)
+
