edb156ed6563411fd1405490369b8b2b01e4ad75
jrodriguez88 <jeferson_88@hotmail.com>
2020-04-23 11:04:53 -0500

    test_parallel

--- make_project_by_date.R
+++ make_project_by_date.R
@@ -20,21 +20,21 @@
 #
 ## function to calculate HUH (growing thermal units) _ tbase,    topt,and thigh depends of crop
 #HUH_cal <- function(tmax, tmin, tbase = 8, topt = 30, thigh = 42.5) {
-    
-    tav <- (tmin + tmax)/2
-    
-    h <- 1:24
-    
-    Td <- tav + (tmax - tmin)*cos(0.2618*(h - 14))/2 
-    
-    huh <- Td %>% enframe(name = NULL, "td") %>%
-        mutate(HUH = case_when(td <= tbase | td >= thigh ~ 0,
-                               td > tbase | td <= topt ~ (td - tbase)/24,
-                               td > topt | td < thigh ~ (topt-(td - topt)*(topt - tbase)/(thigh - topt))/24))
-    
-    sum(huh$HUH)   
-    
-} 
+#    
+#    tav <- (tmin + tmax)/2
+#    
+#    h <- 1:24
+#    
+#    Td <- tav + (tmax - tmin)*cos(0.2618*(h - 14))/2 
+#    
+#    huh <- Td %>% enframe(name = NULL, "td") %>%
+#        mutate(HUH = case_when(td <= tbase | td >= thigh ~ 0,
+#                               td > tbase | td <= topt ~ (td - tbase)/24,
+#                               td > topt | td < thigh ~ (topt-(td - topt)*(topt - tbase)/(thigh - topt))/24))
+#    
+#    sum(huh$HUH)   
+#    
+#} 
 #
 #
 #### set locality and planting_window dates (<= 1 month). require wth_data 
